83d207325bfafd3b93b9f4583862980039e96689
DamianKZK <damian.contreras.pizza@gmail.com>
2025-08-18 12:30:47 -0600

    Add files via upload

---
+++ _and.v
@@ -0,0 +1,10 @@
+//1.Definición del modulo y sus entradas y salidas
+//https://github.com/DamianKZK/ArquitecturaDeComputadoras/upload
+
+module _and(input A, input B, output C);
+//2. Declarar señales/elementos internos
+
+//3.Comportamiento del modulo (asignaciones, instancias y conexiones)
+
+assign C=A&B;
+endmodule
