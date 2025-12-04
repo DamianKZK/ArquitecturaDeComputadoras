5684545fbf02d5376ec3b4084739dd29796e8622
DamianKZK <damian.contreras.pizza@gmail.com>
2025-12-03 18:24:01 -0600

    Add timescale directive and endmodule statement

--- Mux1.v
+++ Mux1.v
@@ -1,3 +1,4 @@
+`timescale 1ns/1ns
 module Mux1(
     input [31:0] aluResult,    // Entrada 0
     input [31:0] memData,      // Entrada 1
@@ -5,4 +6,5 @@ module Mux1(
     output [31:0] writeData    // Salida hacia Banco de Registros
 );
     assign writeData = (memToReg) ? memData : aluResult;
+
 endmodule
